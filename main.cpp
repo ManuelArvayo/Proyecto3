0d884f8e90ca9b57d20a30812b6767f021c670be
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2020-05-28 23:54:02 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include <string>
 #include <fstream>
 #include "Reserva.h"
+#include <assert.h>
 using namespace std;
 
-ostream &operator << (ostream &os, fecha f) {
+ostream &operator << (ostream &os, fecha f)
+{
     os << f.getDia();
     os << "/" << f.nombreMes() << "/" <<f.getAnio()<< endl;
     return os;
 }
 
-istream &operator >> (istream &os, fecha &f) {
+istream &operator >> (istream &os, fecha &f)
+{
     cout << "Dia: ";
     os >> f.dd;
     cout << "Mes: ";
@@ -23,84 +26,86 @@ istream &operator >> (istream &os, fecha &f) {
 int main()
 {
 
-material *mat[30];
-libro libroTemp[30];
-software softwareTemp[30];
-disco discoTemp[30];
-reserva res[60];
+    material *mat[30];
+    libro libroTemp[30];
+    software softwareTemp[30];
+    disco discoTemp[30];
+    reserva res[60];
 
-int idMaterial, atributoUno;
-string titulo, atributoDos;
-char clave;
+    int idMaterial, atributoUno;
+    string titulo, atributoDos;
+    char clave, resp;
 
-int dia, mes, anio, idClient;
+    int dia, mes, anio, idClient;
 
-ifstream arMaterial;
-arMaterial.open("Material.txt");
+    ifstream arMaterial;
+    arMaterial.open("Material.txt");
 
-int contMaterial=0;
-while(arMaterial>>idMaterial>>titulo>>clave>>atributoUno>>atributoDos)
-{
-    clave=tolower(clave);
-    switch (clave)
+    int contMaterial=0;
+    while(arMaterial>>idMaterial>>titulo>>clave>>atributoUno>>atributoDos)
     {
+        clave=tolower(clave);
+        switch (clave)
+        {
         case 'l':
-            {
-                libro temp(idMaterial,titulo,atributoUno,atributoDos);
-                libroTemp[contMaterial]= temp;
-                *(mat+contMaterial) = &libroTemp[contMaterial];
-                contMaterial++;
-                break;
-            }
+        {
+            libro temp(idMaterial,titulo,atributoUno,atributoDos);
+            libroTemp[contMaterial]= temp;
+            *(mat+contMaterial) = &libroTemp[contMaterial];
+            contMaterial++;
+            break;
+        }
         case 'd':
-            {
-                disco temp(idMaterial,titulo,atributoUno,atributoDos);
-                discoTemp[contMaterial]= temp;
-                *(mat+contMaterial)=&discoTemp[contMaterial];
-                contMaterial++;
-                break;
-            }
+        {
+            disco temp(idMaterial,titulo,atributoUno,atributoDos);
+            discoTemp[contMaterial]= temp;
+            *(mat+contMaterial)=&discoTemp[contMaterial];
+            contMaterial++;
+            break;
+        }
         case 's':
-            {
-                software temp(idMaterial,titulo,atributoUno,atributoDos);
-                softwareTemp[contMaterial] = temp;
-                *(mat+contMaterial)=&softwareTemp[contMaterial];
-                contMaterial++;
-                break;
-            }
+        {
+            software temp(idMaterial,titulo,atributoUno,atributoDos);
+            softwareTemp[contMaterial] = temp;
+            *(mat+contMaterial)=&softwareTemp[contMaterial];
+            contMaterial++;
+            break;
+        }
         default:
             cout<<"Error en la captura de Datos (MATERIAL)"<<endl;
+        }
     }
-}
-arMaterial.close();
+    arMaterial.close();
 
-ifstream arReserva;
-arReserva.open("Reserva.txt");
-int contReserva=0;
-while(arReserva>>dia>>mes>>anio>>idMaterial>>idClient)
-{
-    fecha f1(anio,mes,dia);
-    reserva resTemp(idMaterial,idClient,f1);
-    res[contReserva]=resTemp;
-    contReserva++;
-}
-int contReservaTemp = contReserva;
-cout<<"Ingrese A para Consultar la lista de Materiales"<<endl;
-cout<<"Ingrese B para Consultar la lista de reservaciones"<<endl;
-cout<<"Ingrese C para Consultar las reservaciones de un material dado"<<endl;
-cout<<"Ingrese D para Consultar las reservaciones de una fecha dada"<<endl;
-cout<<"Ingrese E para Hacer una reservación"<<endl;
-cout<<"Ingrese F para Terminar"<<endl;
-cin>>clave;
-cout<<endl;
-
-clave=tolower(clave);
-switch (clave)
+    ifstream arReserva;
+    arReserva.open("Reserva.txt");
+    int contReserva=0;
+    while(arReserva>>dia>>mes>>anio>>idMaterial>>idClient)
     {
-    case 'a':
+        fecha f1(anio,mes,dia);
+        reserva resTemp(idMaterial,idClient,f1);
+        res[contReserva]=resTemp;
+        contReserva++;
+    }
+    int contReservaTemp = contReserva-1;
+    do
+    {
+        cout<<"Ingrese A para Consultar la lista de Materiales"<<endl;
+        cout<<"Ingrese B para Consultar la lista de reservaciones"<<endl;
+        cout<<"Ingrese C para Consultar las reservaciones de un material dado"<<endl;
+        cout<<"Ingrese D para Consultar las reservaciones de una fecha dada"<<endl;
+        cout<<"Ingrese E para Hacer una reservación"<<endl;
+        cout<<"Ingrese F para Terminar"<<endl;
+        cin>>clave;
+        cout<<endl;
+
+        clave=tolower(clave);
+        switch (clave)
+        {
+        case 'a':
         {
             cout<<"----------------------LISTA DE MATERIALES----------------------"<<endl;
-            for(int i=0;i<contMaterial;i++)
+            for(int i=0; i<contMaterial; i++)
             {
                 cout<<"MATERIAL #"<<i+1<<endl;
                 mat[i]->muestraDatos();
@@ -108,17 +113,20 @@ switch (clave)
             }
             cout<<"TOTAL DE MATERIALES:  "<<contMaterial<<endl;
             cout<<"---------------------------------------------------------------"<<endl;
+            cout<<endl<<"Desea Realizar otra operacion?"<<endl;
+            cin>>resp;
+
             break;
         }
 
-    case 'b':
+        case 'b':
         {
             cout<<"----------------------LISTA DE RESERVAS----------------------"<<endl;
-            for(int i=0;i<contReserva;i++)
+            for(int i=0; i<contReserva; i++)
             {
-            cout<<"Reserva #"<<i+1<<":"<<endl;
-            cout<<"   Fecha de Inicio:"<<res[i].getFechaReserva();
-                for(int k=0; k<contMaterial;k++)
+                cout<<"Reserva #"<<i+1<<":"<<endl;
+                cout<<"   Fecha de Inicio:"<<res[i].getFechaReserva();
+                for(int k=0; k<contMaterial; k++)
                 {
                     if(res[i].getIdMaterial()==mat[k]->getIdMaterial())
                     {
@@ -126,25 +134,31 @@ switch (clave)
                         cout<<"   Nombre de Material:"<<mat[k]->getTitulo()<<endl;
                     }
                 }
-            cout<<"   Id del Cliente:"<<res[i].getIdCliente()<<endl;
-            cout<<endl;
+                cout<<"   Id del Cliente:"<<res[i].getIdCliente()<<endl;
+                cout<<endl;
             }
             cout<<"TOTAL DE RESERVAS  :"<<contReserva<<endl;
             cout<<"---------------------------------------------------------------"<<endl;
+            cout<<endl<<"Desea Realizar otra operacion?"<<endl;
+            cin>>resp;
+
             break;
+
         }
-    case 'c':
+        case 'c':
         {
             cout<<"Ingrese el ID del material: "<<endl;
             cin>>idMaterial;
+            cout<<endl;
             int x=1;
-            for(int i=0;i<contReserva;i++)
+            cout<<"---------------------------------------------------------------"<<endl;
+            for(int i=0; i<contReserva; i++)
             {
                 if(idMaterial==res[i].getIdMaterial())
                 {
                     cout<<"Reserva #"<<x<<" del material #"<<idMaterial<<":"<<endl;
-                     cout<<"   Fecha de Inicio:"<<res[i].getFechaReserva();
-                    for(int k=0; k<contMaterial;k++)
+                    cout<<"   Fecha de Inicio:"<<res[i].getFechaReserva();
+                    for(int k=0; k<contMaterial; k++)
                     {
                         if(res[i].getIdMaterial()==mat[k]->getIdMaterial())
                         {
@@ -157,10 +171,16 @@ switch (clave)
                     x++;
                 }
             }
+            cout<<"---------------------------------------------------------------"<<endl;
+
+            cout<<endl<<"Desea Realizar otra operacion?"<<endl;
+            cin>>resp;
+
             break;
+
         }
 
-    case 'd':
+        case 'd':
         {
             cout<<"Ingrese el dia: "<<endl;
             cin>>dia;
@@ -170,13 +190,13 @@ switch (clave)
             cin>>anio;
             fecha ftemp(anio,mes,dia);
             int x=1;
-            for(int i=0;i<contReserva;i++)
+            for(int i=0; i<contReserva; i++)
             {
                 if(ftemp==res[i].getFechaReserva())
                 {
                     cout<<"Reserva #"<<x<<" del material #"<<idMaterial<<":"<<endl;
-                     cout<<"   Fecha de Inicio:"<<res[i].getFechaReserva();
-                    for(int k=0; k<contMaterial;k++)
+                    cout<<"   Fecha de Inicio:"<<res[i].getFechaReserva();
+                    for(int k=0; k<contMaterial; k++)
                     {
                         if(res[i].getIdMaterial()==mat[k]->getIdMaterial())
                         {
@@ -189,15 +209,26 @@ switch (clave)
                     x++;
                 }
             }
+            cout<<endl<<"Desea Realizar otra operacion?"<<endl;
+            cin>>resp;
+
+
             break;
+
         }
-    case 'e':
+        case 'e':
         {
             int contMaterialTemp;
-            bool fValido;
+            bool fValido=false;
+            bool verif= false;
+            int reservatemp;
             cout<<"Ingrese el ID del material: "<<endl;
             cin>>idMaterial;
-            //Verificacion
+            for(int k=0; k<contMaterial; k++)            //Verificacion
+                if(idMaterial==mat[k]->getIdMaterial())
+                {
+                    contMaterialTemp =k;
+                }
             cout<<"Ingrese el Id del cliente: "<<endl;
             cin>>idClient;
             cout<<"Ingrese el dia: "<<endl;
@@ -207,61 +238,81 @@ switch (clave)
             cout<<"Ingrese el anio: "<<endl;
             cin>>anio;
             fecha ftemp(anio,mes,dia);
-            reserva r1(idMaterial,idClient,ftemp);
-            for(int k=0; k<contMaterial;k++)
-                if(idMaterial==mat[k]->getIdMaterial())
+            reserva reservaN(idMaterial,idClient,ftemp);
+
+            for(int i=0; i<contReserva; i++)
+            {
+                if(res[i].getIdMaterial()==mat[contMaterialTemp]->getIdMaterial())
                 {
-                    contMaterialTemp =k;
+                    verif=true;
+                    reservatemp=i;
                 }
-            int i=0;
+            }
             fecha temp2= ftemp;
             ftemp+mat[contMaterialTemp]->cantidadDiasPrestamo();
-//            do{
-                    int j=0;
-                    do
-                    {
+            if(verif==true)
+            {
+                fecha inicio;
+                fecha termino;
+                inicio =res[reservatemp].getFechaReserva();
+                termino=res[reservatemp].calculaFechaFinReserva(mat[contMaterialTemp]->cantidadDiasPrestamo());
 
-                        fecha inicio;
-                        fecha termino;
-                        inicio =res[j].getFechaReserva();
-                        termino=res[j].calculaFechaFinReserva(mat[contMaterialTemp]->cantidadDiasPrestamo());
-                        if(res[j].getIdMaterial()==mat[contMaterialTemp]->getIdMaterial())
-                        {
-                        if((temp2>=inicio && temp2<=termino)||(ftemp>=inicio && ftemp<=termino))
-                            fValido=false;
-                        else
-                            fValido=true;
-                        }
-                        j++;
-                    }while(fValido==false && j<contReserva);
-
-//                i++;
-//                }while(fValido==true && i<contReserva);
+                if((temp2>=inicio && temp2<=termino)||(ftemp>=inicio && ftemp<=termino))
+                    fValido=false;
+                else
+                    fValido=true;
+            }
+            if(verif==false)
+                fValido=true;
             if(fValido==false)
                 cout<<"FECHA NO DISPONIBLE"<<endl;
             if(fValido==true)
             {
                 contReservaTemp++;
-                res[contReservaTemp]= r1;
+                res[contReservaTemp]= reservaN;
                 cout<<"RESERVA AGREGADA AL SISTEMA:"<<endl;
                 cout<<"Reserva #"<<contReservaTemp<<endl;
-                cout<<"   Fecha de Inicio:"<<r1.getFechaReserva();
-                cout<<"   Fecha de Termino:"<<r1.calculaFechaFinReserva(mat[contMaterialTemp]->cantidadDiasPrestamo());
+                cout<<"   Fecha de Inicio:"<<reservaN.getFechaReserva();
+                cout<<"   Fecha de Termino:"<<reservaN.calculaFechaFinReserva(mat[contMaterialTemp]->cantidadDiasPrestamo());
                 cout<<"   Nombre de Material:"<<mat[contMaterialTemp]->getTitulo()<<endl;
-                cout<<"   Id del Cliente:"<<r1.getIdCliente()<<endl;
+                cout<<"   Id del Cliente:"<<reservaN.getIdCliente()<<endl;
 
 
             }
+            cout<<endl<<"Desea Realizar otra operacion?"<<endl;
+            cin>>resp;
+            break;
+
+        }
+        case 'f':
+        {
+            ofstream arSalida;
+            arSalida.open("Reserva.txt");
+            for(int i=0; i<=contReservaTemp; i++)
+            {
+                arSalida<<res[i].getFechaReserva().getDia()<<" ";
+                arSalida<<res[i].getFechaReserva().getMes()<<" ";
+                arSalida<<res[i].getFechaReserva().getAnio()<<" ";
+                arSalida<<res[i].getIdMaterial()<<" ";
+                arSalida<<res[i].getIdCliente()<<endl;
+            }
+        }
+        }
+        if(resp=='n')
+        {
+            ofstream arSalida;
+            arSalida.open("Reserva.txt");
+            for(int i=0; i<=contReservaTemp; i++)
+            {
+                arSalida<<res[i].getFechaReserva().getDia()<<" ";
+                arSalida<<res[i].getFechaReserva().getMes()<<" ";
+                arSalida<<res[i].getFechaReserva().getAnio()<<" ";
+                arSalida<<res[i].getIdMaterial()<<" ";
+                arSalida<<res[i].getIdCliente()<<endl;
+            }
         }
 
-}
-
-//
-//                            {cout<<"MATERIAL NO DISPONIBLE"<<endl;}
-//                    ftemp+mat[contMaterialTemp]->cantidadDiasPrestamo();
-//
-//
-//                        else
-//                        {
+    }
+    while(resp=='s');
 
 }
